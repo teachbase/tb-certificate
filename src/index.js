16caaca34b7aefbc3fc29d17fbb20ebf0bab0282
martjushev <martjushev@teachbase.ru>
2022-08-29 22:33:13 +0400

    [TN-7661] install bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import './index.css';
 import store from './redux/store';
